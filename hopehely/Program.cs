32b9c5bed8665bab5ea3d9302f2f8edeeddaf8e2
Adithegood177 <nemethadamnorbert@gmail.com>
2024-12-10 08:22:49 +0100

    Add project files.

---
+++ hopehely/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace hopehely
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> hoElemNev = new List<string>();
+			List<int> hoElemSzam = new List<int>();
+
+            Console.WriteLine("Kérem adja meg a hógömbhöz elemet: ");
+			string nev = Console.ReadLine();
+            Console.WriteLine("Kérem adja meg a hogy hány darabot tegyünk bele: ");
+			int szam = Convert.ToInt32(Console.ReadLine());
+
+        }
+	}
+}
