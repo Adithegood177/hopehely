36cd41aa24008aaf82595b8b31a37f6895d61fa2
Adithegood177 <nemethadamnorbert@gmail.com>
2024-12-10 08:46:25 +0100

    anotherone

--- hopehely/Program.cs
+++ hopehely/Program.cs
@@ -14,6 +14,8 @@
 				try {
 					Console.WriteLine("Kérem adja meg a hógömbhöz elemet: ");
 					 nev = Console.ReadLine();
+					//throw new ArgumentException("Nem lehet üres"); 
+					//string.IsNullOrWhiteSpace
 					if (nev == null)
 					{
 						throw new Exception("Nem jó bemenet");
@@ -22,14 +24,24 @@
 					{
 						throw new Exception("Nem jó bemenet");
 					}
-					Console.WriteLine("Kérem adja meg a hogy hány darabot tegyünk bele: ");
-					szam = Convert.ToInt32(Console.ReadLine());
-
 				}
 				catch(OverflowException) { Console.WriteLine("Hiba"); }
 				catch (FormatException) { Console.WriteLine("Hiba"); }
-				
 				catch(Exception ex) { Console.WriteLine($"{ex.Message}"); }
+
+				try { 
+					Console.WriteLine("Kérem adja meg a hogy hány darabot tegyünk bele: ");
+					szam = Convert.ToInt32(Console.ReadLine());
+					//int count = int.Parse(Console.ReadLine());
+					//throw new ArgumentException("Nem lehet üres"); 
+					if (szam < 1)
+					{
+						throw new Exception("Nagyobb mint 0");
+					}
+				}
+				catch (OverflowException) { Console.WriteLine("Hiba"); }
+				catch (FormatException) { Console.WriteLine("Hiba"); }
+				catch (Exception ex) { Console.WriteLine($"{ex.Message}"); }
 				hoElemNev.Add(nev);
 				hoElemSzam.Add(szam);
 
